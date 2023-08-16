7d1953c72428d3629f00b6a86606d7de73c49cd4
Jayan Ratna <jayanratna@gmail.com>
2023-08-16 16:51:43 +1200

    update to use resend node SDK

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,27 +1,18 @@
 import { NextResponse } from "next/server";
+import { Resend } from "resend";
 
 export const runtime = "edge";
 export const dynamic = "force-dynamic";
 
-const RESEND_API_KEY = "re_123456789";
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
-  const res = await fetch("https://api.resend.com/emails", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${RESEND_API_KEY}`,
-    },
-    body: JSON.stringify({
-      from: "Acme <onboarding@resend.dev>",
-      to: ["delivered@resend.dev"],
-      subject: "hello world",
-      html: "<strong>it works!</strong>",
-    }),
-  });
+  const res = await resend.emails.send({
+    from: "Acme <onboarding@resend.dev>",
+    to: ["delivered@resend.dev"],
+    subject: "hello world",
+    html: "<strong>it works!</strong>",
+  })
 
-  if (res.ok) {
-    const data = await res.json();
-    return NextResponse.json(data);
-  }
+  return NextResponse.json({ id: res.id })
 }
