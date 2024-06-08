25f6f2e39266d4a125700fc4516fc87496689eb1
Lee Robinson <lrobinson2011@gmail.com>
2024-06-08 18:34:14 -0500

    Update example to latest deps.

--- README.md
+++ README.md
@@ -1,35 +1,33 @@
-# Resend with Vercel Edge Functions
+# Resend with Vercel Functions
 
-This example shows how to use Resend with [Vercel Edge Functions](https://vercel.com).
+This example shows how to use Resend with [Vercel Functions](https://vercel.com/docs/functions).
 
 ## Prerequisites
 
 To get the most out of this guide, you’ll need to:
 
-* [Create an API key](https://resend.com/api-keys)
-* [Verify your domain](https://resend.com/domains)
+- [Create an API key](https://resend.com/api-keys)
+- [Verify your domain](https://resend.com/domains)
 
 ## Instructions
 
 1. Install dependencies:
 
-  ```sh
-npm install
-# or
-yarn
-  ```
+```sh
+pnpm install
+```
 
 2. Run Next.js locally:
 
-  ```sh
-npm run dev
-  ```
+```sh
+pnpm run dev
+```
 
 3. Open URL in the browser:
 
-  ```
+```
 http://localhost:3000/api/send
-  ```
+```
 
 ## License
 
--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,27 +1,22 @@
-import { NextResponse } from "next/server";
-
-export const runtime = "edge";
-export const dynamic = "force-dynamic";
-
-const RESEND_API_KEY = "re_123456789";
+const RESEND_API_KEY = 're_123456789';
 
 export async function POST() {
-  const res = await fetch("https://api.resend.com/emails", {
-    method: "POST",
+  const res = await fetch('https://api.resend.com/emails', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
       Authorization: `Bearer ${RESEND_API_KEY}`,
     },
     body: JSON.stringify({
-      from: "Acme <onboarding@resend.dev>",
-      to: ["delivered@resend.dev"],
-      subject: "hello world",
-      html: "<strong>it works!</strong>",
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['delivered@resend.dev'],
+      subject: 'hello world',
+      html: '<strong>it works!</strong>',
     }),
   });
 
   if (res.ok) {
     const data = await res.json();
-    return NextResponse.json(data);
+    return Response.json(data);
   }
 }
