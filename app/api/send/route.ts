1f8206283763fd1bb77fffd3b2c15c416f445997
gabriel miranda <gabrielmfern@outlook.com>
2025-10-31 12:20:38 -0300

    use Resend SDK with env variables

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,22 +1,16 @@
-const RESEND_API_KEY = 're_123456789';
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
-  const res = await fetch('https://api.resend.com/emails', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${RESEND_API_KEY}`,
-    },
-    body: JSON.stringify({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
-      subject: 'hello world',
-      html: '<strong>it works!</strong>',
-    }),
+  const response = await resend.emails.send({
+    from: 'Acme <onboarding@resend.dev>',
+    to: ['delivered@resend.dev'],
+    subject: 'hello world',
+    html: '<strong>it works!</strong>',
   });
 
-  if (res.ok) {
-    const data = await res.json();
-    return Response.json(data);
-  }
+  return Response.json(response, {
+    status: response.error ? 500 : 200,
+  });
 }
